b2f091f1cfb1f6cbcf0e9034656c9860c75c7b02
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2019-03-03 11:43:51 +0100

    Improve Setup

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,12 +1,17 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider(
-    :auth0,
-    ENV['AUTH0_CLIENT_ID'],
-    ENV['AUTH0_CLIENT_SECRET'],
-    ENV['AUTH0_DOMAIN'],
-    callback_path: '/auth/auth0/callback',
-    authorize_params: {
-      scope: 'openid profile'
-    }
-  )
+Rails.application.configure do
+  config.middleware.use OmniAuth::Builder do
+    provider(
+      :auth0,
+      ENV['AUTH0_CLIENT_ID'],
+      ENV['AUTH0_CLIENT_SECRET'],
+      ENV['AUTH0_DOMAIN'],
+      callback_path: '/auth/auth0/callback',
+      authorize_params: {
+        scope: 'openid profile'
+      }
+    )
+  end
+
+  config.auth0_domain = ENVied.AUTH0_DOMAIN
+  config.auth0_client_id = ENVied.AUTH0_CLIENT_ID
 end
--- config/initializers/secret_key_base.rb
+++ config/initializers/secret_key_base.rb
@@ -1,3 +1,1 @@
 Rails.application.config.secret_key_base = ENVied.SECRET_KEY_BASE
-Rails.application.config.auth0_domain = ENVied.AUTH0_DOMAIN
-Rails.application.config.auth0_client_id = ENVied.AUTH0_CLIENT_ID
