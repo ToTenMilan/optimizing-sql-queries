d5f6650175085756795cb6189e01d7bd9d659bf4
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2019-03-03 14:16:22 +0100

    Add sorting

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,8 +1,36 @@
 class AnswersController < ApplicationController
   include Secured
+  helper_method :sort_column, :sort_direction
   
   def index
-    @answers = Answer.all.includes([:learner, { possible_answer: { question: :quiz }}])
-                         .paginate(page: params[:page], per_page: 10)
+    @answers = Answer.all.includes([:learner, { possible_answer: { question: :quiz }}])                     
+                         .paginate(page: params[:page], per_page: 10)             
+                         .order(sort_column + ' ' + sort_direction)
+
+    # @answers = Answer.all.joins(:learner).order(params[:sort])
+
+    # @answers = Answer.all.includes([:learner, { possible_answer: { question: :quiz }}])
+    #                     .joins([:learner, { possible_answer: { question: :quiz }}])
+    #                     .paginate(page: params[:page], per_page: 10)                  
+    #                    #  .order("'#{params[:sort]}'")
+                         
+                          
+                         
+  end
+
+  private
+
+  def sort_column
+    %w[
+      learners.full_name
+      learners.email
+      quizzes.name
+      questions.title
+      possible_answers.value
+    ].include?(params[:sort]) ? params[:sort] : 'name'
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def sortable(column, title)
+    direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
+    link_to title, sort: column, direction: direction
+  end
 end
